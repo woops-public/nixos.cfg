1b3ddaf5eb6685f6469952c4fc01f5ea10b877f1
woops-public <woops-public@posteo.me>
2026-01-07 13:16:33 +0000

    nixos and home-manager refactor

--- flake.nix
+++ flake.nix
@@ -15,12 +15,12 @@
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            ./configuration.nix
+            ./nixos/configuration.nix
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.w0op5 = ./home.nix;
+              home-manager.users.w0op5 = ./home-manager/home.nix;
 
               # Optionally, use home-manager.extraSpecialArgs to pass
               # arguments to home.nix
